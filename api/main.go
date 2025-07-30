7afcf9f77e4b706081f54927d0d80d07121b09f5
abhi9ab <abhinabdas004@gmail.com>
2025-07-30 16:28:34 +0530

    Create Shorten URL Route

--- api/main.go
+++ api/main.go
@@ -1,15 +1,21 @@
 package main
 
+// standard library for printing, logging errors, and accessing environment varibles
 import (
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/abhi9ab/URL-Shortener/routes"
+	// Web framework like Express.
 	"github.com/gofiber/fiber/v2"
+	// Middleware to log HTTP requests.
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	// Loads .env file like dotenv in Node.js
 	"github.com/joho/godotenv"
 )
 
+// registers the routes
 func setupRoutes(app *fiber.App) {
 	app.Get("/:url", routes.ResolveURL)
 	app.Post("/api/v1", routes.ShortenURL)
@@ -20,12 +26,17 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	// Initializes the Fiber web server like app := express() in Node
 	app := fiber.New()
 
-	//app.Use(csrf.New())
+	// app.Use(csrf.New())
+	// Adds logging middleware (like morgan in Express) to log each incoming HTTP request.
 	app.Use(logger.New())
 
+	// Registers your custom routes.
 	setupRoutes(app)
 
+	// Starts the server on the port defined in .env, e.g., APP_PORT=:3000.
 	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
 }
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -16,5 +16,34 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
 
+func ShortenURL(c *fiber.Ctx) error {
+	// check for the incoming request body
+	body := new(request)
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse JSON",
+		})
+	}
 
+	// check if the input is an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid URL",
+		})
+	}
+
+	// check for the domain error
+	// users may abuse the shortener by shorting the domain `localhost:3000` itself
+	// leading to a inifite loop, so don't accept the domain for shortening
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "haha... nice try",
+		})
+	}
+
+	// enforce https
+	// all url will be converted to https before storing in database
+	body.URL = helpers.EnforceHTTP(body.URL)
+}
