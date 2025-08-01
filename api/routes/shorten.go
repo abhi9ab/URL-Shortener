c26cbe78b5c09ca781bd1ee660ca7ab69616cc1c
abhi9ab <abhinabdas004@gmail.com>
2025-08-01 20:20:13 +0530

    Create Rate Limiting

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,7 +1,16 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/abhi9ab/URL-Shortener/database"
+	"github.com/abhi9ab/URL-Shortener/helpers"
+	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type request struct {
@@ -27,6 +36,33 @@ func ShortenURL(c *fiber.Ctx) error {
 		})
 	}
 
+	// The logic uses Redis to limit the number of API calls from a particular IP address (c.IP()) to prevent abuse. 
+	// It works like a token bucket system with a quota and expiry.
+	r2 := database.CreateClient(1)
+	defer r2.Close() // Closes the Redis client automatically at the end of the function to release resources.
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val) // Converts the quota from string (Redis stores as string) to integer.
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
+
+	// check if the input is an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid URL",
+		})
+	}
+
+
 	// check if the input is an actual URL
 	if !govalidator.IsURL(body.URL) {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -46,4 +82,6 @@ func ShortenURL(c *fiber.Ctx) error {
 	// enforce https
 	// all url will be converted to https before storing in database
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	r2.Decr(database.Ctx, c.IP())
 }
