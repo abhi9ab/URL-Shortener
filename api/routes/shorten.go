81013877257d49f985f50bef5e936cbc84a5e6af
abhi9ab <abhinabdas004@gmail.com>
2025-08-01 22:54:03 +0530

    Create Docker Container

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -83,5 +83,51 @@ func ShortenURL(c *fiber.Ctx) error {
 	// all url will be converted to https before storing in database
 	body.URL = helpers.EnforceHTTP(body.URL)
 
+		// check if the user has provided any custom dhort urls
+	// if yes, proceed,
+	// else, create a new short using the first 6 digits of uuid
+	// haven't performed any collision checks on this
+	// you can create one for your own
+	var id string
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	// check if the user provided short is already in use
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "URL short already in use",
+		})
+	}
+	if body.Expiry == 0 {
+		body.Expiry = 24 // default expiry of 24 hours
+	}
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Unable to connect to server",
+		})
+	}
+	// respond with the url, short, expiry in hours, calls remaining and time to reset
+	resp := response{
+		URL:             body.URL,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
 	r2.Decr(database.Ctx, c.IP())
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+	return c.Status(fiber.StatusOK).JSON(resp)
 }
